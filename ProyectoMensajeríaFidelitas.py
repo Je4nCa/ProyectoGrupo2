0dab0f1a97a7e9efa919e5376db7cde88dda3f5e
Fiomm15 <145303535+Fiomm15@users.noreply.github.com>
2023-11-26 21:20:19 -0600

    actualizacion3Fio ProyectoMensajeríaFidelitas.py

--- ProyectoMensajeríaFidelitas.py
+++ ProyectoMensajeríaFidelitas.py
@@ -72,3 +72,61 @@ def rastrear_paquetes():
         print(f"Estado actual del paquete {usuario_id}: {paquetes[usuario_id]['Estado']}")
     else:
         print("Número de paquete no válido. Registre el paquete primero.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Menú principal
+while True:
+    print("Menú:")
+    print("1. Registrar usuario del comercio")
+    print("2. Crear paquete para envío")
+    print("3. Registrar Factura Electrónica")
+    print("4. Rastrear paquete")
+    print("5. Estadísticas generales")
+    print("6. Salir")
+ 
+    opcion = input("Seleccione una opción: ")
+ 
+    if opcion == '1':
+        registrar_usuario()
+    elif opcion == '2':
+        correo_usuario = input("Ingrese el correo electrónico del usuario del comercio: ")
+        if correo_usuario in usuarios:
+            crear_paquete(correo_usuario)
+        else:
+            print("Correo electrónico no válido. Registre el usuario del comercio primero.")
+    elif opcion == '3':
+        registrar_factura()
+    elif opcion == '4':
+        rastrear_paquetes()
+    elif opcion == '5':
+        estadisticas()
+    elif opcion == '6':
+        print("Guardando datos en el archivo 'datosmensajeriafidelitas.txt'")
+        break
+    else:
+        print("Opción no válida. Por favor, seleccione una opción válida.")
+
+tiene menú contextual
